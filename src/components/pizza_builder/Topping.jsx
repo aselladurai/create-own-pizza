102462c621657e8e72535341686367304654e8cb
aselladurai <aselladurai@mdsol.com>
2020-04-27 17:21:08 -0700

    added toppings files

---
+++ src/components/pizza_builder/Topping.jsx
@@ -0,0 +1,129 @@
+import React, { Fragment, useState, useEffect } from 'react';
+import '../../style/style.css';
+import cheese_moz from '../../assets/images/pizza_type/icon_cheese_Mozzarella.webp';
+import objPizzaBuilder from '../../assets/labels/labels';
+
+import btnLeftDisabled from '../../assets/images/toppings/btn-topping-left-inactive.png';
+import btnLeftEnabled from '../../assets/images/toppings/btn-topping-left-active.png'
+import btnRightDisabled from '../../assets/images/toppings/btn-topping-right-inactive.png'
+import btnRightEnabled from '../../assets/images/toppings/btn-topping-right-active.png'
+import btnWholeDisabled from '../../assets/images/toppings/btn-topping-whole-inactive.png'
+import btnWholeEnabled from '../../assets/images/toppings/btn-topping-whole-active.png'
+
+const Topping = (props) => { 
+
+    const [toppingInfo, setToppingInfo] = useState({
+        Id: props.Id,
+        Image: props.imageUrl,
+        Name: props.toppingName,
+        Desc: props.toppingDesc,
+        IsSelected: props.isSelected,
+        Amount: 'Regular', //Extra/Regular/None
+        LRW: props.toppingLRW // L/R/W
+    })
+
+    useEffect(() => {
+        props.updateParent(toppingInfo);
+
+    }, [toppingInfo.IsSelected, toppingInfo.Amount, toppingInfo.LRW])
+
+    const isSelected = toppingInfo.IsSelected;
+    const toppingItemClassName = isSelected ? 'toppingItem-selected' : 'toppingItem-unselected'
+    const toppingImageClassName = isSelected ? 'toppingImage-selected' : 'toppingImage-unselected'
+
+    
+
+    const extraClickHandler = (e) => {
+        setToppingInfo({
+            ...toppingInfo,
+            Amount: 'Extra'
+        })
+    }
+
+    const regularClickHandler = () => {
+        setToppingInfo({
+            ...toppingInfo,
+            Amount: 'Regular'
+        })
+    }
+
+    const noneClickHandler = () => {
+        setToppingInfo({
+            ...toppingInfo,
+            Amount: 'None'
+        })
+    }
+
+    const toppingItemClickHandler = (e) => {
+        let className = e.target.className;
+        let extra = className.indexOf('btn-topping-extra');
+        let regular = className.indexOf('btn-topping-regular');
+        //let none = className.indexOf('btn-topping-none');
+        let lrw = className.indexOf('lrw');
+        let btnControls = className.indexOf('toppingBtnControls');
+        let imgControls = className.indexOf('toppingImgControls');
+
+        let isItemClick = (extra === -1 && regular === -1 && lrw === -1 && btnControls === -1 && imgControls === -1);
+        /*let leftHalf = className.indexOf('btn-topping-none');
+        let rightHalf = className.indexOf('btn-topping-none');
+        let whole = className.indexOf('btn-topping-none');*/
+
+        if(isItemClick){ //not clicked on buttons & img controls
+            setToppingInfo({
+                ...toppingInfo,
+                IsSelected: !toppingInfo.IsSelected
+            })
+        }
+    }
+
+    const leftHalfClickHandler = () => {
+        setToppingInfo({
+            ...toppingInfo,
+            LRW: 'L'
+        })
+    }
+
+    const rightHalfClickHandler = () => {
+        setToppingInfo({
+            ...toppingInfo,
+            LRW: 'R'
+        })
+    }
+
+    const wholeClickHandler = () => {
+        setToppingInfo({
+            ...toppingInfo,
+            LRW: 'W'
+        })
+    }
+
+    return (
+        
+        <div className={toppingItemClassName} onClick={toppingItemClickHandler}>
+            <div className="toppingImageContainer">
+                <img className={toppingImageClassName} src={toppingInfo.Image} alt="Mushroom"/>
+                { isSelected ? <i className="fas fa-check-circle"></i> : ''}
+            </div>
+            <div className="toppingInfo">
+                <div className="toppingName">{toppingInfo.Name}</div>
+                { isSelected ?  <div className="toppingDesc">{toppingInfo.Desc}</div> : '' }
+            </div>
+            { isSelected ? 
+                <Fragment>
+                    <div className="toppingBtnControls">
+                        <button className={`btn-topping-extra btn btn-outline-secondary ${(toppingInfo.Amount === 'Extra') ? 'selected' : ''}`} onClick={extraClickHandler}>Extra</button>
+                        <button className={`btn-topping-regular btn btn-outline-secondary ${(toppingInfo.Amount === 'Regular') ? 'selected' : ''}`} onClick={regularClickHandler}>Regular</button>
+                        <button className={`btn-topping-none btn btn-outline-secondary ${(toppingInfo.Amount === 'None') ? 'selected' : ''}`} onClick={noneClickHandler}>None</button>
+                    </div>
+                    <div className="toppingImgControls">
+                        {toppingInfo.LRW === 'L' ? <img src={btnLeftEnabled} onClick={leftHalfClickHandler} className="l-enabled lrw" alt="Left enabled"/> : <img src={btnLeftDisabled} onClick={leftHalfClickHandler} className="l-disabled lrw" alt="Left disabled"/>}
+                        {toppingInfo.LRW === 'R' ? <img src={btnRightEnabled} onClick={rightHalfClickHandler} className="r-enabled lrw" alt="Right enabled"/> : <img src={btnRightDisabled} onClick={rightHalfClickHandler} className="r-disabled lrw" alt="Right disabled"/>}
+                        {toppingInfo.LRW === 'W' ? <img src={btnWholeEnabled} onClick={wholeClickHandler} className="w-enabled lrw" alt="Whole enabled"/> : <img src={btnWholeDisabled} onClick={wholeClickHandler} className="w-disabled lrw" alt="Whole disabled"/>}
+                    </div>
+                </Fragment>
+            : '' }
+        </div>
+    )
+}
+
+export default Topping;
---
+++ src/components/pizza_builder/ToppingList.jsx
@@ -0,0 +1,142 @@
+import React, { Fragment, useState, useEffect } from 'react';
+import '../../style/style.css';
+import cheese_moz from '../../assets/images/pizza_type/icon_cheese_Mozzarella.webp';
+import objPizzaBuilder from '../../assets/labels/labels';
+import Topping from './Topping';
+import mushroom_icon from '../../assets/images/toppings/icon_topping_Mushroom.webp';
+import spinach_icon from '../../assets/images/toppings/icon_topping_Spinach_fresh.webp';
+import onion_icon from '../../assets/images/toppings/icon_topping_Onion_Red.webp';
+import olive_icon from '../../assets/images/toppings/icon_topping_Olive_black.webp';
+import greenpepper_icon from '../../assets/images/toppings/icon_topping_GreenBellPepper.webp';
+import bananapepper_icon from '../../assets/images/toppings/icon_topping_BananaPepper.webp';
+import pineapple_icon from '../../assets/images/toppings/icon_topping_Pineapple.webp';
+import jalapeno_icon from '../../assets/images/toppings/icon_topping_Jalapeno.webp';
+import tomato_icon from '../../assets/images/toppings/icon_topping_Tomato.webp';
+import mushroom_reg from '../../assets/images/toppings/topping_Mushroom_ML_reg.webp'
+
+const ToppingList = (props) => { 
+    const objToppingList = [
+        {
+            Id: 'MUSHROOM',
+            iconImage: mushroom_icon,
+            toppingName: objPizzaBuilder.objToppings.lblMushrooms,
+            toppingDesc: '',
+            isSelected: false,
+            Amount: '',
+            toppingLRW: 'L'
+        },
+        {
+            Id: 'SPINACH',
+            iconImage: spinach_icon,
+            toppingName: objPizzaBuilder.objToppings.lblRoastedSpinach,
+            toppingDesc: '',
+            isSelected: true,
+            Amount: '',
+            toppingLRW: 'R'
+        },
+        {
+            Id: 'ONION',
+            iconImage: onion_icon,
+            toppingName: objPizzaBuilder.objToppings.lblRedOnions,
+            toppingDesc: '',
+            isSelected: false,
+            Amount: '',
+            toppingLRW: 'W'
+        },
+        {
+            Id: 'OLIVE',
+            iconImage: olive_icon,
+            toppingName: objPizzaBuilder.objToppings.lblBlackOlives,
+            toppingDesc: '',
+            isSelected: false,
+            Amount: '',
+            toppingLRW: 'L'
+        },
+        {
+            Id: 'GREEN_PEPPER',
+            iconImage: greenpepper_icon,
+            toppingName: objPizzaBuilder.objToppings.lblGreenPeppers,
+            toppingDesc: '',
+            isSelected: false,
+            Amount: '',
+            toppingLRW: 'R'
+        },
+        {
+            Id: 'BANANA_PEPPER',
+            iconImage: bananapepper_icon,
+            toppingName: objPizzaBuilder.objToppings.lblBananaPeppers,
+            toppingDesc: '',
+            isSelected: false,
+            Amount: '',
+            toppingLRW: 'W'
+        },
+        {
+            Id: 'PINEAPPLE',
+            iconImage: pineapple_icon,
+            toppingName: objPizzaBuilder.objToppings.lblPineapple,
+            toppingDesc: '',
+            isSelected: false,
+            Amount: '',
+            toppingLRW: 'L'
+        },
+        {
+            Id: 'JALAPENO',
+            iconImage: jalapeno_icon,
+            toppingName: objPizzaBuilder.objToppings.lblJalapenoPeppers,
+            toppingDesc: '',
+            isSelected: false,
+            Amount: '',
+            toppingLRW: 'R'
+        },
+        {
+            Id: 'TOMATO',
+            iconImage: tomato_icon,
+            toppingName: objPizzaBuilder.objToppings.lblRomaTomatoes,
+            toppingDesc: '',
+            isSelected: false,
+            Amount: '',
+            toppingLRW: 'W'
+        }
+    ]
+
+    const [toppingList, setToppingList] = useState({
+        ...objToppingList
+    })
+    
+    useEffect(() => {
+        
+    }, [])
+    const updatePizzaBuilder = (toppingInfo) => {
+        let list = toppingList;
+        for(let item in list) {
+            if(list[item].Id === toppingInfo.Id) {
+                list[item].isSelected = toppingInfo.IsSelected
+                list[item].toppingLRW = toppingInfo.LRW
+                list[item].Amount = toppingInfo.Amount
+            }
+        }
+        setToppingList({
+            ...list
+        })
+        props.updatePB(toppingList);
+    }
+    
+    return (
+        <div className="toppingList">
+            {
+                objToppingList.map((toppingItem, i) => {
+                    return <Topping key={i} 
+                    Id={toppingItem.Id}
+                    imageUrl={toppingItem.iconImage} 
+                    toppingName={toppingItem.toppingName} 
+                    toppingDesc={toppingItem.toppingDesc} 
+                    isSelected={toppingItem.isSelected}
+                    toppingLRW={toppingItem.toppingLRW} 
+                    updateParent={updatePizzaBuilder}/>
+                })
+            }
+        </div>
+    )
+}
+
+export default ToppingList;
